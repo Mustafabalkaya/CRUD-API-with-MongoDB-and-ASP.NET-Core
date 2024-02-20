06ca7d147b0940c8ea30b21b1e8658934dd1a606
INFOTOOLSSV <infotoolssv@gmail.com>
2024-02-19 21:12:14 -0600

    Update

--- obj/Debug/net8.0/API_CRUD_MongoDB.AssemblyInfo.cs
+++ obj/Debug/net8.0/API_CRUD_MongoDB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_CRUD_MongoDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2659320153eb6c7726dc25bd6dcc34c70d53453b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_CRUD_MongoDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_CRUD_MongoDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
